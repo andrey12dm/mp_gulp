67bc104b3115f00ba97d66c747c43120ef443f04
Andrey Dmitrenko <91805979+andrey12dm@users.noreply.github.com>
2024-03-30 21:26:00 +0200

    new

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -7,3 +7,24 @@ navBtn.onclick = function () {
     nav.classList.toggle('header__top-row--mobile');
     document.body.classList.toggle('no-scroll');
 }
+;(() => {
+	const refs = {
+		openModalBtn: document.querySelector('[data-modal-open]'),
+		closeModalBtn: document.querySelector('[data-modal-close]'),
+		modal: document.querySelector('[data-modal]'),
+	}
+
+	refs.openModalBtn.addEventListener('click', toggleModal)
+	refs.closeModalBtn.addEventListener('click', toggleModal)
+
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden')
+	}
+})()
+console.log('work')
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+})

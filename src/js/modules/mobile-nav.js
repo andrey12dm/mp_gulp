f41883cd723110d9d22738b6adacf60e44292757
Andrey Dmitrenko <91805979+andrey12dm@users.noreply.github.com>
2024-04-20 23:08:59 +0300

    added edits and made a burger menu

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,10 +1,10 @@
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
-const nav = document.querySelector('.header__top-row');
+const nav = document.querySelector('.header__nav-btn');
 
 navBtn.onclick = function () {
     navIcon.classList.toggle('nav-icon--active');
-    nav.classList.toggle('header__top-row--mobile');
+    nav.classList.toggle('header__nav-btn--mobile');
     document.body.classList.toggle('no-scroll');
 }
 ;(() => {
